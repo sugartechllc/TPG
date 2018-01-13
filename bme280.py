f501a32b86e97de6c1d5ccb9a5e0121147aed531
sugartechllc <sugartechllc@gmail.com>
2018-01-13 10:42:35 -0700

    New bme280 class.

---
+++ bme280.py
@@ -0,0 +1,28 @@
+"""
+BME280 (I2C) abstraction
+"""
+from Adafruit_BME280 import *
+
+# pylint: disable=C0103
+# pylint: disable=C0325
+
+class bme280(object):
+    def __init__(self):
+        self.sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+    def reading(self):
+        """
+        Return a hash containing sensor readings.
+        The hash will contain:
+           temp_C
+           press_mb
+           rh
+        """
+        retval = {}
+        retval["temp_C"] = self.sensor.read_temperature()
+        retval["pres_mb"] = self.sensor.read_pressure()/100
+        retval["rh"] = sensor.read_humidity()
+
+if __name__ == '__main__':
+    bme280 = bme280()
+    print(bme280.reading())
